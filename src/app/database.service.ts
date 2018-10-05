3d626e162fb7fdfab95b350aa1869a30241ddc5f
cobaltblu27 <cobaltblu27@gmail.com>
2018-10-05 19:44:26 +0900

    added messageservice

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { tap } from 'rxjs/operators'
+import { Observable, of } from 'rxjs'
+import { MessageService } from './message.service'
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) { }
 }
